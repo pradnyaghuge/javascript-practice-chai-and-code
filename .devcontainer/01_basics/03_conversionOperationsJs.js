33b4674d13a074a2632f7807ee303c0c473588b0
pradnyaghuge <69901011+pradnyaghuge@users.noreply.github.com>
2024-01-06 11:22:37 +0000

    conversion of dattatypes

---
+++ .devcontainer/01_basics/03_conversionOperationsJs.js
@@ -0,0 +1,31 @@
+// let score="34abs"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// console.log(typeof score);
+
+// let valueNumber=Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+//33==>number
+//33abc==>NaN
+//true=>1 and false==> 0
+
+
+let isLoggedIn="pradnya"
+
+let testisLoggedin=Boolean(isLoggedIn)
+// console.log(testisLoggedin);
+
+// 1==>true
+// 0==>false
+// " "==>empty==>false
+// "pradya"==>true
+
+let someNumber=56
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
