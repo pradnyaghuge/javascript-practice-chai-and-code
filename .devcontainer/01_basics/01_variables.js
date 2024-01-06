bf2a93ac3f28eeb9cc08d025c3be6ab6c15cc726
pradnyaghuge <69901011+pradnyaghuge@users.noreply.github.com>
2024-01-06 10:35:56 +0000

    datat

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,22 @@
+console.log("hi");
+
+const accountId = "14425;";
+
+let accountEmail = "uid@mail.com";
+
+var accountPwd = "67789";
+accountCity = "Nashik";
+
+// var accountId=5// not alllowed
+
+/* prefer not use var because its not good pracice
+
+*/
+accountCity = "banglore";
+accountEmail = "abc@mail.com";
+accountPwd = "77584";
+console.log(accountId);
+
+let accountState;
+
+console.table([accountEmail, accountPwd, accountCity, accountState]);
