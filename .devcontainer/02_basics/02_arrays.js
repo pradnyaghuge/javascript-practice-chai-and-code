08c0d877c041fda50560b74913879585a333d334
pradnyaghuge <69901011+pradnyaghuge@users.noreply.github.com>
2024-01-07 19:56:23 +0000

    arrays part 2

---
+++ .devcontainer/02_basics/02_arrays.js
@@ -0,0 +1,46 @@
+//Arrays part 2
+
+const marvel_heros=["thor","Ironman","spiderman"]
+
+const dc_heros=["superman","batman","flash"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// marvel_heros.concat(dc_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][2]);
+
+// const allHeros=marvel_heros.concat(dc_heros)
+
+// console.log(allHeros);
+
+// const all_new_heros=[...marvel_heros,...dc_heros]
+// console.log(all_new_heros);
+
+// const anotherArray=[2,4,6,[4,6],44,[243,[543,4],2]];
+
+// const realAnotherArray=anotherArray.flat(Infinity);
+// console.log(realAnotherArray);
+
+// console.log(Array.isArray("pradnya"));
+// console.log(Array.from("pradnya"));
+// console.log(Array.from({name:"pradnya"}));// intresting case for interview
+
+let score1=678
+let score8=958
+let score2=278
+let score5=178
+
+console.log(Array.of(score1,score2,score8,score5));
+
+
+
+
+
+
+
+
+
+
