2babe44ad91865b762901a8c9412cef98cd37bc9
pradnyaghuge <69901011+pradnyaghuge@users.noreply.github.com>
2024-01-07 09:00:01 +0000

    strings

--- .devcontainer/01_basics/06_string.js
+++ .devcontainer/01_basics/06_string.js
@@ -1,7 +1,45 @@
-console.log("pradnya"+"ghuge");
-console.log(`my name is ${"pradnya"} and surname is ${"ghuge"}`);
+// console.log("pradnya"+"ghuge");
 
-const gameName= new String("counterstrick")
+// console.log(`my name is ${"pradnya"} and surname is ${"ghuge"}`);
 
-console.log(gameName.__proto__.);
-console.log(gameName[0]);
+
+
+// console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf("e"));
+
+// const newString=gameName.substring(0,4);
+// console.log(newString);
+// const gameName= new String("pradnya")
+
+// // console.log(gameName.length);
+
+// const anotherString=console.log(gameName.slice(-4 ));
+
+// const newString1=" pradnyaghuge  "
+// console.log(newString1);
+// console.log(newString1.trim());
+
+// const name="ghuge"
+// // console.log(name.slice(-5,2));
+
+// // console.log(name.slice(-1,-4));
+// console.log(name.slice(-4,-1));
+
+// const url="pradnyaghuge.co"
+// // console.log(url.replace("co","com"));
+// console.log(url.includes("pradnya"));
+
+const splitString="Hi all lets master react"
+const arr=splitString.split(" ")
+console.log(splitString)
+
+
+
+
+
+
+
+
