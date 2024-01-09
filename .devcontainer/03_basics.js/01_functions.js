1f5571cd5c55dacd248d7c8e4a5d8e0657089f7d
pradnyaghuge <69901011+pradnyaghuge@users.noreply.github.com>
2024-01-09 20:16:22 +0000

    function basics

---
+++ .devcontainer/03_basics.js/01_functions.js
@@ -0,0 +1,30 @@
+
+
+// function sayHi(){
+//     console.log("P");
+//     console.log("R");
+//     console.log("A");
+//     console.log("D");
+//     console.log("N");
+//     console.log("Y");
+//     console.log("A");
+// }
+
+// sayHi()
+
+// function add(number1,number2){
+//     console.log(number1+number2);
+// }
+
+function add(number1,number2){
+    // const result=number1+number2
+    // return result;//  nothing will print after result, return is last keyword for any function
+    return number1+number2
+}
+let result=add(7,9)
+console.log("result is: "+result);
+// add(7,"9") //o/p: 79
+
+// const result=add(7,9)
+
+
